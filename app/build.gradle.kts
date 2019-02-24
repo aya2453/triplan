2add9b7bb2cc6f19f42281b478374eb73dc8a78b
aya2453 <aya.nk.0116@gmail.com>
2019-02-25 01:10:39 +0900

    Version up dependencies libralies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,11 @@
-import com.android.build.gradle.ProguardFiles.getDefaultProguardFile
-import org.gradle.internal.impldep.com.amazonaws.PredefinedClientConfigurations.defaultConfig
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
-import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 
 plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
     kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
 }
 
 android {
@@ -47,6 +45,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.0.2")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
     implementation("com.google.android.material:material:1.0.0")
+    implementation("android.arch.navigation:navigation-fragment:1.0.0-rc01")
+    implementation("android.arch.navigation:navigation-ui:1.0.0-rc01")
 
     // Firebase
     implementation("com.google.firebase:firebase-core:16.0.7")
@@ -56,8 +56,10 @@ dependencies {
     // Dagger
     implementation("com.google.dagger:dagger-android:2.21")
     implementation("com.google.dagger:dagger-android-support:2.21")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
     kapt("com.google.dagger:dagger-compiler:2.21")
     kapt("com.google.dagger:dagger-android-processor:2.21")
+
 
     // AAC
     implementation("androidx.lifecycle:lifecycle-extensions:2.0.0")
@@ -66,10 +68,10 @@ dependencies {
     // Util
     implementation("androidx.core:core-ktx:1.0.1")
 
-    implementation ("com.squareup.retrofit2:retrofit:2.4.0")
-    implementation ("com.squareup.moshi:moshi:1.8.0")
-    implementation ("com.squareup.retrofit2:converter-moshi:2.4.0")
-    kapt ("com.squareup.moshi:moshi-kotlin-codegen:1.7.0")
+    implementation("com.squareup.retrofit2:retrofit:2.5.0")
+    implementation("com.squareup.moshi:moshi:1.8.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.5.0")
+    kapt("com.squareup.moshi:moshi-kotlin-codegen:1.8.0")
 
 
     testImplementation("junit:junit:4.12")
--- build.gradle.kts
+++ build.gradle.kts
@@ -4,9 +4,10 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.0")
-        classpath(kotlin("gradle-plugin", version = "1.3.11"))
+        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath(kotlin("gradle-plugin", version = "1.3.21"))
         classpath("com.google.gms:google-services:4.2.0")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-rc01")
     }
 }
 
